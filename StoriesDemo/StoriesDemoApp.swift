01ae769cc75fa431e9c50eedfcefbe6ac0a7e509
Aydincan Ataberk <ataberk@gmail.com>
2021-09-20 16:32:09 +0300

    Initial Commit

---
+++ StoriesDemo/StoriesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesDemoApp.swift
+//  StoriesDemo
+//
+//  Created by C. Aydincan Ataberk on 20.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct StoriesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
